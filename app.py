40a219542ac0051e42e30234f6c9b23372917941
kzz54tzg5r-code <kzz54tzg5r@privaterelay.appleid.com>
2026-06-18 10:59:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,2 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import plotly.express as px
-from io import BytesIO
-import re
-
-st.set_page_config(
-    page_title="ORION PRO",
-    page_icon="🚀",
-    layout="wide"
-)
